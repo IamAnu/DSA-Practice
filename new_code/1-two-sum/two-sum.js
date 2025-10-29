7cbd422a797e91517a6df459ad06f2c0a09ae4bf
Anupriya Pandey <90721325+IamAnu@users.noreply.github.com>
2025-10-29 23:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 58.8 MB (5.24%) - LeetSync

--- new_code/1-two-sum/two-sum.js
+++ new_code/1-two-sum/two-sum.js
@@ -6,15 +6,13 @@
 var twoSum = function(arr, target) {
     let map = new Map();
     for(let i=0;i<arr.length;i++){
-        map.set(arr[i], i);
-    }
-    for(let i=0;i<arr.length;i++){
-        const rem_elem = target-arr[i]
-        if(map.has(rem_elem) && map.get(rem_elem) !== i){
-            return[i,map.get(rem_elem)];
-        }
+        let rem = target-arr[i];
+     if( map.has(rem)) {
+        return [map.get(rem), i]
+     }  map.set(arr[i], i)
     }
     return [];
 };
 
-// Took help from Leetcode solution
+
+//  one pass method
