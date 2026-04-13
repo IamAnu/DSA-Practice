901af6e6024e0abfe6c7ab2d8641280cb3795efe
Anupriya Pandey <90721325+IamAnu@users.noreply.github.com>
2026-04-13 18:19:47 +0530

    Time: 1 ms (86.13%) | Memory: 55.3 MB (39.51%) - LeetSync

--- new_code/1-two-sum/two-sum.js
+++ new_code/1-two-sum/two-sum.js
@@ -4,15 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(arr, target) {
-    let map = new Map();
+    map = new Map ();
     for(let i=0;i<arr.length;i++){
-        let rem = target-arr[i];
-     if( map.has(rem)) {
-        return [map.get(rem), i]
-     }  map.set(arr[i], i)
+        let diff = target-arr[i];
+        if(map.has(diff)){
+            return [map.get(diff), i];
+        }
+        else{
+            map.set(arr[i], i);
+        }
     }
-    return [];
 };
-
-
-//  one pass method
