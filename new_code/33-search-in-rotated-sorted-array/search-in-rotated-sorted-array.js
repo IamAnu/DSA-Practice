9f6d5f8f7ac7418f55c08c7be692a1ac25095eab
Anupriya Pandey <90721325+IamAnu@users.noreply.github.com>
2026-04-21 13:53:24 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (51.50%) - LeetSync

---
+++ new_code/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    // let ans = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] === target) return i;
+    }
+    return -1;
+};
